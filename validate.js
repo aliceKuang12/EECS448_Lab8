b9cec3bfd88f7ac6f6f62f5ccc3df766ff501f3c
Alice <66434350+aliceKuang12@users.noreply.github.com>
2022-04-17 22:52:32 -0500

    Update Password validate to indicate success

--- validate.js
+++ validate.js
@@ -2,11 +2,16 @@ function validate() {
   var v1 = document.getElementById("p1").value;
   var v2 = document.getElementById("p2").value;
 
+
   if(v1 != v2) {
     alert("Password don't match!");
   }
 
-  if( v1.length < 8 ||v2.length < 8)  {
-    alert("Your password is too short.")
+  else if( v1.length < 8 ||v2.length < 8)  {
+    alert("Password too short, must be at least 8 characters.")
+  }
+  else
+  {
+	  alert("Success, valid password!")
   }
 }
